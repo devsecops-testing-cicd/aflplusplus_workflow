efe6350b76f086a17f041f74e86aba99f6435f1d
rapt0r <goktugserez@gmail.com>
2024-02-04 19:55:48 +0900

    feat: introduce afl actions

---
+++ aflplusplus_run/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+set -eu
+
+ROOT_DIR="afl"
+
+CAMPAIGN_NAME=$1
+FUZZ_TARGET=$2
+INPUT_FOLDER=$3
+TIMEOUT=$4
+TIME=$5
+QEMU_MODE=$6
+
+if [ "$QEMU_MODE" == true ]; then
+	QFLAG="-Q"
+else
+	QFLAG=""
+fi
+
+mkdir -p "$CAMPAIGN_NAME/output"
+
+afl-fuzz -M $CAMPAIGN_NAME -t $TIMEOUT $QEMU_MODE -i $INPUT_FOLDER -o "$CAMPAIGN_NAME/output" $FUZZ_TARGET @@ 
+
