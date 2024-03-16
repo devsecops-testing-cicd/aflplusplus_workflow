a223eba6ccf7add66871c437bd286e2fd71ad5f6
rapt0r <goktugserez@gmail.com>
2024-03-16 21:53:19 +0900

    fix(aflplusplus_build/entrypoint.sh): fix cmake source path

--- aflplusplus_build/entrypoint.sh
+++ aflplusplus_build/entrypoint.sh
@@ -28,13 +28,15 @@ case "$COMPILER" in
         ;;
 esac
 
+# Change directory to source
+cd $SOURCE_DIR
 
 # Create build directory
 mkdir -p build
 cd build
 
 # Configure project with AFL++ compiler
-cmake -DCMAKE_C_COMPILER="$AFL_COMPILER" -DCMAKE_CXX_COMPILER="$AFL_COMPILER_CXX" "$SOURCE_DIR"
+cmake -DCMAKE_C_COMPILER="$AFL_COMPILER" -DCMAKE_CXX_COMPILER="$AFL_COMPILER_CXX" ..
 
 # Build project
 make
